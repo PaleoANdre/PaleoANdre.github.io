bb1861d675b9cfb543da408c09f3173e122a77c4
PaleoANdre <144718709+PaleoANdre@users.noreply.github.com>
2024-04-29 14:53:16 +0000

    Task 2 done

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,13 @@ for (let i = 1; i <= 10; i++){
 }
 console.log("Task 2");
 for (let i = 5; i >= 1; i--){
-    console.log("Blastoff!")
+    console.log(i);
 }
+console.log("Blastoff!")
 console.log("Task 3");
-
+for (let i = 1; i <= 50; i++){
+    
+}
 console.log("Task 4");
 
 console.log("Task 5");
