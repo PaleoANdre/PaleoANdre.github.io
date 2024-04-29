f50e609bdd208f68845c97c388e713df7eda47f7
PaleoANdre <144718709+PaleoANdre@users.noreply.github.com>
2024-04-29 17:09:39 +0000

    Task 3 completed succesfully!

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,22 @@ console.log("Task 1");
 for (let i = 1; i <= 10; i++){
     console.log(i*i);
 }
+
 console.log("Task 2");
 for (let i = 5; i >= 1; i--){
     console.log(i);
 }
 console.log("Blastoff!")
-console.log("Task 3");
-for (let i = 1; i <= 50; i++){
-    
-}
-console.log("Task 4");
 
+console.log("Task 3");
+for (let i = 0; i <= 50; i+=2){
+    console.log(i);
+}
+
+console.log("Task 4");
+sumAllTheseotherNumberspls = 0;
+for (let i = 1; i <= 100; i++){
+    sumAllTheseotherNumberspls = sumAllTheseotherNumberspls + i;
+    console.log("" + sumAllTheseotherNumberspls)
+}
 console.log("Task 5");
