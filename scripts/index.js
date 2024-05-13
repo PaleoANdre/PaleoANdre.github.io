fbf34ee215d08756eb9f2f15b0912de2e99046ad
PaleoANdre <144718709+PaleoANdre@users.noreply.github.com>
2024-05-13 17:12:18 +0000

    dynamic content

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
-let arr = [red, orange, yellow, green, aqua, fuchsia];
+let colorList = ["red", "orange", "yellow", "green", "deepskyblue", "rebbecapurple", "fuchsia"];
+let rainbowLetters = document.createElement('ul');
+for (let i = 0; i < colorList.length; i++){
+    let rainbow = document.createElement('li');
+    rainbowLetters.appendChild(rainbow);
+}
+document.body.appendChild(rainbowLetters);
