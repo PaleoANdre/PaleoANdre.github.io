9e43a1c15a903b047632847a41fa9a9329cfe7f7
PaleoANdre <144718709+PaleoANdre@users.noreply.github.com>
2024-05-14 23:50:02 +0000

    Dynamic content

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,8 @@ let rainbowLetters = document.createElement('ul');
 for (let i = 0; i < colorList.length; i++){
     let rainbow = document.createElement('li');
     rainbowLetters.appendChild(rainbow);
+    rainbow.innerText = colorList[i];
+    rainbow.style.color = colorList[i];
+    
 }
 document.body.appendChild(rainbowLetters);
